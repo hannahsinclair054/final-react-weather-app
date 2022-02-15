8d55eed4618a151aa29e810598dedb6b085df31d
hannahsinclair054 <88463868+hannahsinclair054@users.noreply.github.com>
2022-02-15 15:14:49 +0000

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Hannah Sinclair and is{" "}
+        <a
+          href="https://github.com/hannahsinclair054/final-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
